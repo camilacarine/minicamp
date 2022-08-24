14b152c017b3e80c4a8c659f738a6061e09b1616
camilacarine <ccmr.tech@gmail.com>
2022-08-23 23:12:34 -0300

    study: Make an IMC calculator

---
+++ teste-pratico-imc2/js/script.js
@@ -0,0 +1,53 @@
+function start(){
+    var buttonCalculateImc = document.querySelector('#button-calculate-imc');
+    buttonCalculateImc.addEventListener('click', handleButtonClick);
+
+    var inputWeight = document.querySelector('#input-weight');
+    var inputHeight = document.querySelector('#input-height');
+
+    inputWeight.addEventListener('input', handleButtonClick)
+    inputHeight.addEventListener('input', handleButtonClick)
+
+    handleButtonClick();
+}
+function calculateImc(weight, height){
+    return weight/ (height * height);
+}
+function handleButtonClick(){
+    var inputWeight = document.querySelector('#input-weight');
+    var inputHeight = document.querySelector('#input-height');
+    var imcResult = document.querySelector('#imc-result');
+    var imcClass = document.querySelector('#imc-class');
+
+    var weight = Number(inputWeight.value);
+    var height = Number(inputHeight.value);
+
+    var imc = calculateImc(weight, height);    
+    var formattedImc = imc.toFixed(2).replace('.',',');
+    var imcFaixa = imc_classification(imc)
+        
+    imcResult.textContent = formattedImc;
+    imcClass.textContent = imcFaixa;
+}
+
+function imc_classification(imc) {    
+     if (imc >= 16 && imc < 17) {            
+            return ' Muito abaixo do peso'
+        } else if (imc >= 17 && imc < 18.5) {           
+            return ' Abaixo do peso'
+        } else if (imc >= 18.5 && imc < 25) {
+            return ' Peso Normal'
+        } else if (imc >= 25 && imc < 30) {
+            return ' Acima do peso'
+        } else if (imc >= 30 && imc <35) {
+            return ' Obesidade Grau I'          
+        } else if (imc >= 35 && imc < 40) {
+            return ' Obesidade Grau II'
+        } else if (imc > 40) {
+            return ' Obesidade Grau III'
+        } else {
+            return ' Inválido'
+        }   
+}
+
+start();
